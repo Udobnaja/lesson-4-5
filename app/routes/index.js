162ff1e3fb1da27fc829e0c4ecf8434230fdf052
udobnaja <udobnaja@gmail.com>
2018-03-22 00:12:26 +0300

    Удаление checkout & Получение только хешей

--- app/routes/index.js
+++ app/routes/index.js
@@ -50,8 +50,6 @@ indexRouter
                 }).catch((error) => {
                     renderErrorPage({res, router: 'term', error});
                 });
-
-            // promise with finally will be sexy
 });
 
 indexRouter.route('/branch')
@@ -66,49 +64,15 @@ indexRouter.route('/branch')
             });
     });
 
-const SEPARATOR = Math.random()*10e8;
-
 indexRouter.route('/branch/:name')
     .get((req, res) => {
 
-        Exec(`git checkout ${req.params.name}`, options)
-            .then(()=> {
-            /* тут можно вставить формат даты ? в надежде что он валидный */
-                return Exec(`
-                git log --pretty=format:'{
-                    "hash":"%h",
-                    "author":"%an",
-                    "date":"%ar",
-                    "body":"%s"} ${SEPARATOR}'`, options);
-
-            }).then((log) => {
-            /* JSON */
-               let chunk = log.split(SEPARATOR);
-               let commits = [];
-
-               for (let stringObject of chunk){
-                   if (stringObject.length === 0) continue;
-                   let object;
-                   try {
-                       object = JSON.parse(stringObject);
-                   } catch(e) {
-                       let strings = stringObject.match(/".*":".*"/g);
-                       let newObject = {};
-                       for (let str of strings){
-                           let position = str.indexOf(':');
-
-                           let key = str.slice(1, position - 1);
-                           let value = str.slice(position + 2, str.length - 1);
-                           newObject[key] = value;
-                       }
-                       object = newObject;
-                   }
-                   commits.push(object);
-               }
-
+        Exec(`git log ${req.params.name} --pretty=format:'%H'`, options)
+            .then((log) => {
+                let hashes = log.split('\n');
 
                 res.render('branch-detail', {
-                    commits
+                    commits: hashes
                 })
             })
             .catch((error) => {
