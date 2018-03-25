da313cd3208d5a91c2738c7ff8afb8d872159373
udobnaja <udobnaja@gmail.com>
2018-03-26 01:22:12 +0300

    test на child exec

---
+++ test/utils/exec.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const expect = chai.expect;
+const exec = require('../../app/utils/child_process/exec');
+
+describe('Exec util', () => {
+    it('Should be equal current working dirrectory', () => {
+        exec('pwd').then((pwd) => {
+            expect(pwd).to.equal(process.cwd());
+        }).catch(() => {
+
+        });
+    });
+});
