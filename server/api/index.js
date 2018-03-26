da972ecba809bf1886c53b423ab1f4e10214a344
udobnaja <udobnaja@gmail.com>
2018-03-27 01:02:35 +0300

    Вынесла получение комитов ветки в отдельные методы, которые можно потом протестить

--- server/api/index.js
+++ server/api/index.js
@@ -4,11 +4,51 @@ const options = config.setting.exec.options;
 const readDir = require('../utils/fs/readdir');
 
 
-exports.getFileContent = async ({name}) => {
+const getFileContent = async ({name}) => {
     return await exec(`git cat-file -p ${name}`, options);
 };
 
-exports.getBranchList = async () => {
+const getBranchList = async () => {
     return await readDir(`${options.cwd}/${config.setting.git}refs/heads/`);
+};
+
+const getCommitsListHash = async ({branch}) => {
+    let hashListString = await exec(`git log ${branch} --pretty=format:'%H'`, options);
+    return hashListString.split('\n');
+};
+
+const getCommitInfo = async ({hash}) => {
+    return await exec(`git show --quiet --pretty=' 
+       Author: %an 
+       Date: %ar 
+       Commit message: %s' ${hash}`, options);
+};
+
+
+const getFormattedCommitList = async ({branch}) => {
+    return await getCommitsListHash({branch})
+        .then((hashList) => {
+            const promises = [];
+
+            for (let hash of hashList) {
+                promises.push(getCommitInfo({hash})
+                    .then((info) => {
+                        return {hash, info};
+                    }).catch(e => e));
+            }
+
+            return Promise.all([...promises]);
+
+        }).catch((error) => {
+            return error;
+        });
+};
+
+module.exports = {
+    getFileContent,
+    getBranchList,
+    getCommitsListHash,
+    getCommitInfo,
+    getFormattedCommitList
 };
 
--- server/controllers/branch.js
+++ server/controllers/branch.js
@@ -1,10 +1,7 @@
 const renderError = require('../helpers/renderError');
 const config = require('../config');
 const {defaultBranch} = config;
-const options = config.setting.exec.options;
-const readDir = require('../utils/fs/readdir');
-const exec = require('../utils/child_process/exec');
-const {getBranchList} = require('../api/index');
+const {getBranchList, getFormattedCommitList} = require('../api/index');
 
 exports.renderBranchList = (req, res) => {
     getBranchList()
@@ -19,32 +16,11 @@ exports.renderBranchList = (req, res) => {
 };
 
 exports.renderBranchDetail = (req, res) => {
-    exec(`git log ${req.params.name} --pretty=format:'%H'`, options)
-        .then((log) => {
-            const hashes = log.split('\n');
-            const promises = [];
-
-            for (let hash of hashes) {
-                promises.push(
-                    new Promise((resolve, reject) => {
-                        exec(`git show --quiet --pretty=' 
-                                Author: %an 
-                                Date: %ar 
-                                Commit message: %s' ${hash}`, options)
-                            .then((info) => {
-                                resolve({hash, info});
-                            })
-                            .catch(reject);
-                    }));
-            }
-
-            Promise.all([...promises]).then((commits) => {
-                res.render('branch-detail', {
-                    commits,
-                    branch: req.params.name
-                });
-            }).catch((error) => {
-                renderError({res, router: 'branch-detail', error});
+    getFormattedCommitList({branch: req.params.name})
+        .then((commits) => {
+            res.render('branch-detail', {
+                commits,
+                branch: req.params.name
             });
         })
         .catch((error) => {
