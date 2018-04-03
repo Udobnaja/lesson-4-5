b34b3952f470111f427bac5a6e8a31834e08a4d4
udobnaja <udobnaja@gmail.com>
2018-04-03 03:05:05 +0300

    обработка ошибки для exec, выставление в exclude файлов для nyc конфигов

--- test/unit/utils/exec.js
+++ test/unit/utils/exec.js
@@ -1,5 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
+chai.should();
+chai.use(require('chai-as-promised'));
 const exec = require('../../../server/utils/child_process/exec');
 
 describe('Проверка работы Утилиты Exec', () => {
@@ -7,5 +9,9 @@ describe('Проверка работы Утилиты Exec', () => {
         exec('pwd').then((pwd) => {
             expect(pwd).to.equal(process.cwd());
         }).catch(e => e);
+    });
+
+    it('Должна выбрасывать ошибку, если, например, такой команды не существует', async () => {
+        await exec('not exist').should.be.rejectedWith(Error);
     });
 });
